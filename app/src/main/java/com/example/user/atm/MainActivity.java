c82611081f8769228f33cdb656677f4e95d82785
Ruby <ruby10387@gmail.com>
2018-10-30 15:38:40 +0800

    first commit

---
+++ app/src/main/java/com/example/user/atm/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.atm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_LOGIN = 100;
+    boolean login = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (!login) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent,RC_LOGIN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_LOGIN) {
+            if(resultCode != RESULT_OK) {
+                finish();
+            }
+        }
+    }
+}
